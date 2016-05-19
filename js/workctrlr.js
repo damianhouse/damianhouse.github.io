54f6cd389f47be5112b4b24fa3f26e21a69d305b
damianhouse <damianhouse@gmail.com>
2016-05-19 17:06:24 -0400

    Add media queries css file. Start angular carousel.

--- js/workctrlr.js
+++ js/workctrlr.js
@@ -1,16 +1,23 @@
 portfolio.controller('WorkController', [ '$location', '$scope' ,'$animate', function($location, $scope, $animate){
 
-
-  // $scope.goHome = function() {
-  //   $location.path('/site');
-  // };
-  //
-  // $scope.goContact = function() {
-  //   $location.path('/contact');
-  // };
-  //
-  // $scope.goWork = function() {
-  //   $location.path('/work');
-  // }
+  $scope.slides = [
+            {image: 'img/trind-landing.jpg', description: 'Landing Page'},
+            {image: 'img/trind-index.jpg', description: 'Event Posts'},
+            {image: 'img/trind-details.jpg', description: 'Post Details'},
+            {image: 'img/trind-conversations.jpg', description: 'Conversations'},
+        ];
+        $scope.currentIndex = 0;
+         $scope.setCurrentSlideIndex = function (index) {
+             $scope.currentIndex = index;
+         };
+         $scope.isCurrentSlideIndex = function (index) {
+             return $scope.currentIndex === index;
+         };
+         $scope.prevSlide = function () {
+             $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+         };
+         $scope.nextSlide = function () {
+             $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+         };
 
 }]);
